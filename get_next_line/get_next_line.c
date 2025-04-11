e52ecb6c7577f577477178d71ccd2a18e3566f3c
nelly <nellyahia@protonmail.com>
2025-04-11 03:46:28 +0200

    Revert "Delete get_next_line directory"
    This reverts commit f953a35851e10b1a0d2a203ff4342020fc77791c.

---
+++ get_next_line/get_next_line.c
@@ -0,0 +1,26 @@
+#include "get_next_line.h"
+
+char	*fill_buffer(char *line)
+{
+	char	*buffer;
+	size_t	i;
+	ssize_t bytes;
+
+	bytes = read(fd, 
+	while (i < BUFFER_SIZE )
+	{
+		buffer[i] = line[i];
+		i++;
+	}
+}
+
+char	*fill_line()
+{
+&& line[i] != '\0' && line[i] != '\n'
+
+}
+
+char	*get_next_line(int fd)
+{
+	
+}
