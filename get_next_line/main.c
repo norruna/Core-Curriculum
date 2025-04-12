19382f6ac9cad8821cece00ce4f2cb31213dfa7a
nelly <nelly@ubuntu.de>
2025-04-12 02:49:08 +0200

    managed to read whole file no leaks yiiy

--- get_next_line/main.c
+++ get_next_line/main.c
@@ -6,7 +6,7 @@
 /*   By: mayahiao <mayahiao@student.42-berlin.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/12 01:04:30 by nelly             #+#    #+#             */
-/*   Updated: 2025/04/12 02:32:50 by mayahiao         ###   ########.fr       */
+/*   Updated: 2025/04/12 02:47:49 by mayahiao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,50 @@
 #include <string.h>
 
 #define BUFFER_SIZE 1024
+
+int	ft_strlen(const char *str)
+{
+	int	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+
+}
+static char	*ft_strcat(char *dest, const char *src)
+{
+	int	i;
+	int	j;
+
+	j = 0;
+	i = ft_strlen(dest);
+	while (src[j])
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	int		i;
+
+	i = 0;
+	str = (char *) malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (!str)
+		return (0);
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	str[i] = '\0';
+	ft_strcat(str, s2);
+	return (str);
+}
 
 int main(void)
 {
@@ -29,23 +73,28 @@ int main(void)
         return (0);
     else
         printf("file opened yiiy\n");
-    static char *line = NULL;
 	bytes = read (fd, buffer, count);
 	printf("bytes read : %ld\n",bytes);
-	if (bytes == -1)
+	if (bytes == -1 || bytes == 0)
     	return (0);
-	char *line_read = (char *) malloc(sizeof(char) * (bytes + 1));
-   if (!line_read)
-		return (0);
-    int	j = 0;
-	while (j < bytes)
+	while (bytes > 0)
 	{
-		line_read[j] = buffer[j];
-		printf("%c",line_read[j]);
-		j++;
+		bytes = read (fd, buffer, count);
+		char *line_read = (char *) malloc(sizeof(char) * (bytes + 1));
+		if (!line_read)
+			return (0);
+    	int	j = 0;
+		while (j < bytes)
+		{
+			line_read[j] = buffer[j];
+			printf("%c",line_read[j]);
+			j++;
+		}
+		line_read[j] = '\0';
+		//static char *line = NULL;
+		//line = ft_strjoin(line, buffer);
+    	printf("\n");
+    	free(line_read);
 	}
-	line_read[j] = '\0';
-    printf("\n");
-    free(line_read);
-    return(0);
+	return(0);
 }
