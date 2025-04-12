fe9a6eb07d938eed289035be637660f17a82c984
nelly <nelly@ubuntu.de>
2025-04-12 02:33:55 +0200

    more practice

--- get_next_line/main.c
+++ get_next_line/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nelly <nelly@student.42.fr>                +#+  +:+       +#+        */
+/*   By: mayahiao <mayahiao@student.42-berlin.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/12 01:04:30 by nelly             #+#    #+#             */
-/*   Updated: 2025/04/12 01:32:30 by nelly            ###   ########.fr       */
+/*   Updated: 2025/04/12 02:32:50 by mayahiao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,37 +14,38 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <fcntl.h>
+#include <string.h>
+
+#define BUFFER_SIZE 1024
 
 int main(void)
 {
-    FILE    *file;
     int     fd;
     size_t  count = 25;
     ssize_t bytes;
-
+    char    buffer[BUFFER_SIZE + 1];
     fd = open("file.txt", O_RDONLY);
     if (fd == -1)
         return (0);
     else
         printf("file opened yiiy\n");
-    static char *buffer;
-    bytes = read (fd, buffer, count);
-    printf("bytes read : %ld\n",bytes);
-    char *line = (char *)malloc (sizeof(char) * (count + 1));
-    if (!line)
-        return NULL;
-    if (bytes == -1)
-    {
-        free (buffer);
-        return (0);
-    }
-    int i = 0;
-    while (buffer[i])
-    {
-        printf("%c",buffer[i]);
-        i++;
-    }
+    static char *line = NULL;
+	bytes = read (fd, buffer, count);
+	printf("bytes read : %ld\n",bytes);
+	if (bytes == -1)
+    	return (0);
+	char *line_read = (char *) malloc(sizeof(char) * (bytes + 1));
+   if (!line_read)
+		return (0);
+    int	j = 0;
+	while (j < bytes)
+	{
+		line_read[j] = buffer[j];
+		printf("%c",line_read[j]);
+		j++;
+	}
+	line_read[j] = '\0';
     printf("\n");
-    free(buffer);
+    free(line_read);
     return(0);
 }
