eda8cd192f89dfb0d973d010fec89d7296b9b894
Manel Yahiaoui <mayahiao@c4b5c6.42berlin.de>
2025-05-14 18:25:49 +0200

    all good still needs cleaning

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -12,7 +12,7 @@
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFFER_SIZE 500 //comment when testing
+# define BUFFER_SIZE 5 //comment when testing
 
 # include <fcntl.h>
 # include <string.h>
@@ -24,9 +24,9 @@ char	*get_next_line(int fd);
 char	*ft_strjoin(char const *s1, char const *s2);
 char    *ft_strcat(char *dest, const char *src);
 int     ft_strlen(const char *str);
-char	*read_buffer(int fd, char *buffer,  char *line, char *extracted_line);
-char	*extract_line(char *line, char *extracted_line);
-int     ft_strchr(char *s, int c);
+char	*read_buffer(int fd, char *line);
+char	*extract_line(char *line);
+char    *ft_strchr(const char *s, int c);
 char	*ft_substr(char const *s1, int start, size_t len);
 char	*ft_strdup(const char *s);
 #endif
