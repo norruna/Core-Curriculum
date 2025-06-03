ad77e61688a5aea9fdbb17b688b26b9f0969906e
nelly <nellyahia@protonmail.com>
2025-06-03 17:04:11 +0200

    slow but sure progress

--- main.c
+++ main.c
@@ -1,23 +1,24 @@
 #include "ft_printf.h"
 
-int ft_printf(const char *, ...)
+int ft_printf(const char *str, ...)
 {
-
-
+	va_list args;
+	va_start(args, str);
+	int	i = 0;
+	char	b;
+	while (str[i] != '\0')
+	{
+		b = va_arg(str[i],int);
+		ft_putchar_fd(1, str[i]);
+	 	i++;
+	
+	}
+	va_end(args);
+	return (i); //number of char printed
 }
 
 int	main(void)
-{
-	const char *str = "Join this % this one";
-	const char *str2 = "(with)";
-	char *c = ft_strchr(str, '%');
-	printf ("c : %s \n\n", c);
-	char *str3 = ft_strjoin(str2, c + 1);
-	printf ("str3 : %s \n\n", str3);
-	char *sub = ft_substr(str,0,ft_strlen(c));
-	printf ("sub : %s \n\n", sub);
-	char *result = ft_strjoin(sub,str3); 
-	printf ("result : %s \n\n", result);
-	
+{	int c;
+	int num = ft_printf("hello there\n", c);
 	return (0);
 }
