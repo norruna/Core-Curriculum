a3f55ec4924f53382347d68b9e155f93f1050395
nelly <nellyahia@protonmail.com>
2025-06-04 01:27:59 +0200

    late night commits|

--- main.c
+++ main.c
@@ -3,14 +3,31 @@
 int ft_printf(const char *str, ...)
 {
 	va_list args;
+	va_list copy;
 	va_start(args, str);
 	int	i = 0;
-	char	b;
-	while (str[i] != '\0')
+	const char	*s = str;
+	//va_copy(copy,args);
+	while (s[i] != '\0')
 	{
-		b = va_arg(str[i],int);
-		ft_putchar_fd(1, str[i]);
-	 	i++;
+		if (s[i] == '%')
+		{	
+			if (s[i + 1] == 's')
+			{	
+
+				ft_putstr_fd(va_arg(args,char *),1);
+				i = i + ft_strlen(;
+				break;
+			}
+			else if (s[i + 1] == '%')
+			{
+				ft_putchar_fd('%', 1);
+				i = i + 2;
+			}
+		}
+		ft_putchar_fd(s[i], 1);
+		//printf("%c",s[i]);
+		i++;	
 	
 	}
 	va_end(args);
@@ -18,7 +35,7 @@ int ft_printf(const char *str, ...)
 }
 
 int	main(void)
-{	int c;
-	int num = ft_printf("hello there\n", c);
+{	char  *c = "there\n";
+	int num = ft_printf("hello %%wor%s ld\n", c);
 	return (0);
 }
