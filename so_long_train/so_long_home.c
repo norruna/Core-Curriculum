8e80b496c1e056b1639fc9402d82e931da3af8fa
nelly <nellyahia@protonmail.com>
2025-07-17 16:20:12 +0200

    code

--- so_long_train/so_long_home.c
+++ so_long_train/so_long_home.c
@@ -6,7 +6,7 @@
 /*   By: mayahiao <mayahiao@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 16:21:34 by mayahiao          #+#    #+#             */
-/*   Updated: 2025/07/15 16:47:21 by mayahiao         ###   ########.fr       */
+/*   Updated: 2025/07/17 01:36:50 by mayahiao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,16 @@
 #include <stdlib.h>
 #include <stdio.h> //for printf just testing
 #include <X11/keysym.h> //for events
+#include "get_next_line.h"
 
+# define TILE 128
 
-/*****************************/
-/*---------------------------*/
-/* WINDOW -------- MANAGEMENT*/
-/*---------------------------*/
-/*****************************/
+//keep track of player position
+typedef struct s_position
+{
+	int		x;
+	int		y;
+}	t_position;
 
 //struct to handle both connection and window pointers
 //more for mlx key hook
@@ -29,7 +32,159 @@ typedef struct s_mlx_param
 {
 	void	*ptr;
 	void	*window;
+	void	*img;
 }	t_mlx_param;
+/*****************************/
+/*---------------------------*/
+/*--MAP----------MANAGEMENT--*/
+/*---------------------------*/
+/*****************************/
+
+int	count_map_lines(char *file)
+{
+	int		count;
+	char	*line;
+	int		fd;
+	fd = open(file, O_RDONLY);
+	if (fd < 0)
+		return (0);
+	count = 0;
+	line = get_next_line(fd);
+	while (line != NULL)
+	{
+		count++;
+		free(line);
+		line = get_next_line(fd);
+	}
+	close(fd);
+	return (count);
+}
+
+void	free_map(char	**map, int i)
+{
+	while (i >= 0)
+	{
+		free(map[i]);
+		i--;
+	}
+	free(map);
+}
+
+char	**parse_map(char *file)
+{
+	char	**map;
+	int		count;
+	int		i;
+	int		j;
+	int		k;
+	char	*line;
+	int		fd;
+
+	fd = open(file, O_RDONLY);
+	if (fd < 0)
+		return (NULL);
+	count = count_map_lines(file);
+	map = malloc ((count + 1) * sizeof(char *));
+	if(!map)
+	{ 
+		close(fd);
+		return (NULL);
+	}
+	i = 0;
+	while (i < count)
+	{
+		line = get_next_line(fd);
+		if (!line)
+			{	
+				free_map(map, i - 1);
+				close(fd);
+				return (NULL);
+			}
+		map[i] = malloc(sizeof(char) * (strlen(line) + 1));
+			if (!map[i])
+			{	
+				free_map(map, i - 1);
+				free(line);
+				close(fd);
+				return (NULL);
+			}
+		k = 0;
+		j = 0;
+		while (line[k] && line[k] != '\n')
+		{
+			if (line[k] == '\n')
+			{
+				break;
+			}	
+			map[i][j] = line[k];
+			j++;
+			k++;
+		}
+		map[i][j] = '\0';
+		i++;
+		free(line);
+	}
+	map[i] = 0;
+	close(fd);
+	return(map);
+}
+
+
+
+void	draw_map(char **map, t_mlx_param *param)
+{
+	int	i;
+	int	j;
+	int	height = TILE;
+	int	width = TILE;
+	void *img_wall = mlx_xpm_file_to_image(param->ptr, "../images/Wall.xpm", &height, &width);
+    void *img_player = mlx_xpm_file_to_image(param->ptr, "../images/Player.xpm", &height, &width);
+    void *img_dad = mlx_xpm_file_to_image(param->ptr, "../images/Dad.xpm", &height, &width);
+    void *img_bed = mlx_xpm_file_to_image(param->ptr, "../images/Bed.xpm", &height, &width);
+	void *img_grass = mlx_xpm_file_to_image(param->ptr, "../images/Grass.xpm", &height, &width);
+
+	if (!img_bed || !img_dad || !img_player || !img_wall)
+	{
+		printf ("couldnt find image\n"); //replace later with ftprintf
+		return ;
+	}
+	i =0;
+	while (map[i])
+	{
+		int j = 0;
+		while(map[i][j])
+		{
+			if (map[i][j] == '1')
+				mlx_put_image_to_window(param->ptr, param->window, img_wall, j * TILE,i * TILE);
+			else if (map[i][j] == 'P')
+				mlx_put_image_to_window(param->ptr, param->window, img_player, j * TILE,i * TILE);
+			else if (map[i][j] == 'C')
+				mlx_put_image_to_window(param->ptr, param->window, img_dad, j * TILE,i * TILE);
+			else if (map[i][j] == 'E')
+				mlx_put_image_to_window(param->ptr, param->window, img_bed, j * TILE,i * TILE);
+			else if (map[i][j] == '0')
+				mlx_put_image_to_window(param->ptr, param->window, img_grass, j * TILE,i * TILE);
+			j++;
+		}
+		i++;
+	}
+	mlx_destroy_image(param->ptr, img_wall);
+    mlx_destroy_image(param->ptr, img_dad);
+    mlx_destroy_image(param->ptr, img_bed);
+    mlx_destroy_image(param->ptr, img_player);
+	mlx_destroy_image(param->ptr, img_grass);
+}
+/*****************************/
+/*---------------------------*/
+/*--MOVEMENT-----MANAGEMENT--*/
+/*---------------------------*/
+/*****************************/
+
+/*****************************/
+/*---------------------------*/
+/* WINDOW -------- MANAGEMENT*/
+/*---------------------------*/
+/*****************************/
 
 int key_presses(int keysym, t_mlx_param *param)
 {
@@ -37,10 +192,7 @@ int key_presses(int keysym, t_mlx_param *param)
 	if (keysym == XK_Escape)
 	{
 		printf("You pressed the ESC key, exiting ...\n");
-		mlx_destroy_window(param->ptr, param->window);
-		mlx_destroy_display(param->ptr);
-		free(param->ptr);
-		exit(0); //to avoid seg faulting at exit
+		mlx_loop_end(param->ptr); //doesnt segfault and also doesnt leak like exit(0)
 	}
 	printf("Thanks for pressing the %d key, nothing happened yet ..\n", keysym);
 	return (0);
@@ -48,20 +200,37 @@ int key_presses(int keysym, t_mlx_param *param)
 
 void init_and_open_window(t_mlx_param *param)
 {
+	
+	char **map = parse_map("map1.ber"); //var for map
+	int	map_height = count_map_lines("map1.ber");
+	int	map_width = ft_strlen(map[0]); // assuming rectangular map
+	int height = map_height * TILE;
+	int	width = map_width * TILE;
 	param->ptr = mlx_init(); //connect to server
 	if (param->ptr == NULL)
 		return ; //error since xvar use malloc
-	param->window = mlx_new_window(param->ptr, 500, 500, "night_routine");
+
+	param->window = mlx_new_window(param->ptr, width, height, "night_routine");
 	if (param->window == NULL)
 	{
-		mlx_destroy_display(param->ptr); //close window
 		free(param->ptr); //close connection
 		return ;
 	}
+	param->img = mlx_xpm_file_to_image(param->ptr , "../images/backgroundtest.xpm" , &width, &height); //putting backgroun image
+	if (!param->img)
+	{
+		mlx_destroy_window(param->ptr, param->window); //close window
+		free(param->ptr); //close connection
+		return ; //error loading the image
+	}
+	mlx_put_image_to_window(param->ptr, param->window, param->img, 0,0);
+	draw_map(map, param);
 	mlx_key_hook(param->window,key_presses,param); //handle key presses
 	mlx_loop(param->ptr);
+	mlx_destroy_image(param->ptr, param->img);
 	mlx_destroy_window(param->ptr, param->window);
 	mlx_destroy_display(param->ptr); //destroy the display
+	free_map(map, count_map_lines("map1.ber") - 1);
 	free(param->ptr); //free xvar struct
 }
 
---
+++ so_long_train/test.c
@@ -0,0 +1,194 @@
+
+#include "get_next_line.h"
+
+int	count_map_lines(char *file)
+{
+	int		count;
+	char	*line;
+	int		fd;
+	fd = open(file, O_RDONLY);
+	if (fd < 0)
+		return (0);
+	count = 0;
+	line = get_next_line(fd);
+	while (line != NULL)
+	{
+		count++;
+		free(line);
+		line = get_next_line(fd);
+	}
+	close(fd);
+	return (count);
+}
+/*not the correct version, correct version is on 42cluster*/
+int	is_rectangular(char *file)
+{
+	int		expected_len;
+	int		fd;
+	char	*line;
+
+	fd = open(file, O_RDONLY);
+	if (fd < 0)
+		return (0);
+	line = get_next_line(fd);
+	if (!line)
+		return (0);
+	if (line[ft_strlen(line) - 1] == '\n') // remove newline
+		line[ft_strlen(line) - 1] = '\0';
+	expected_len = ft_strlen(line);
+	free(line);
+	line = get_next_line(fd);
+	while (line)
+	{
+		if (line[ft_strlen(line) - 1] == '\n')
+			line[ft_strlen(line) - 1] = '\0';
+		if (ft_strlen(line) != expected_len)
+		{
+			free(line);
+			close(fd);
+			return (0);
+		}
+		free(line);
+		line = get_next_line(fd);
+	}
+	close(fd);
+	return (1);
+}
+
+int	is_surrounded_by_walls(char *file)
+{
+	int		count;
+	char	*line;
+	int		fd;
+	int		i;
+	int		j;
+
+	i = 0;
+	fd = open(file, O_RDONLY);
+	if (fd < 0)
+		return (0);
+	count = count_map_lines(file);
+	line = get_next_line(fd);
+	while (i < count)
+	{
+		if (i == 0 || i == count - 1)
+		{
+			j = 0;
+			while (line[j] && line[j] != '\n')
+			{
+				if (line[j] != '1')
+				{
+					free(line);
+					close(fd);
+					return (0);
+				}
+				j++;
+			}
+		}
+		free(line);
+		line = get_next_line(fd);
+		i++;
+	}
+	close(fd);
+	return (1);
+}
+void	free_map(char	**map, int i)
+{
+	while (i >= 0)
+	{
+		free(map[i]);
+		i--;
+	}
+	free(map);
+}
+
+char	**parse_map(char *file)
+{
+	char	**map;
+	int		count;
+	int		i;
+	int		j;
+	int		k;
+	char	*line;
+	int		fd;
+
+	fd = open(file, O_RDONLY);
+	if (fd < 0)
+		return (NULL);
+	count = count_map_lines(file);
+	map = malloc ((count + 1) * sizeof(char *));
+	if(!map)
+	{ 
+		close(fd);
+		return (NULL);
+	}
+	i = 0;
+	while (i < count)
+	{
+		line = get_next_line(fd);
+		if (!line)
+			{	
+				free_map(map, i - 1);
+				close(fd);
+				return (NULL);
+			}
+		map[i] = malloc(sizeof(char) * (strlen(line) + 1));
+			if (!map[i])
+			{	
+				free_map(map, i - 1);
+				free(line);
+				close(fd);
+				return (NULL);
+			}
+		k = 0;
+		j = 0;
+		while (line[k])
+		{
+			map[i][j] = line[k];
+			j++;
+			k++;
+		}
+		map[i][j] = '\0';
+		i++;
+		free(line);
+	}
+	map[i] = 0;
+	close(fd);
+	return(map);
+}
+
+#include <stdio.h>
+int	main(void)
+{
+	//THE LEAKS ARE COMING FROM MAIN FUNCTION IGNORING SINCE ITS FOR TESTING
+	char	**map;
+	char *file = "map1.ber";
+	int count = is_rectangular(file);
+	if (count == 1)
+		printf("map is rectangular :) \n");
+	else
+		printf("map is not rectangular :(\n");
+	int	index;
+	index = is_surrounded_by_walls(file);
+	if (index == 0)
+		printf("the map is not surrounded by walls :(\n");
+	else
+		printf("the map is surrounded by walls :)\n");
+	map = parse_map(file);
+	int i =0;
+	while (map[i])
+	{
+		int j = 0;
+		while(map[i][j])
+		{
+			printf("%c,",map[i][j]);
+			j++;
+		}
+		printf("\n");
+		i++;
+	}
+	free_map(map, count);
+	return (0);
+
+}
+
